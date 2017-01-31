16687aa3dd0c166ae1de48a1c1b0c9e6ea13b781
Reece <reece@steadlane.com.au>
2017-01-31 13:09:35 +1000

    Added RowSize to Field Model

--- code/models/Vision6Field.php
+++ code/models/Vision6Field.php
@@ -1,6 +1,29 @@
 <?php
 
-class Vision6Field extends DataObject {
+/**
+ * Class Vision6Field
+ *
+ * @author Reece Alexander <reece@steadlane.com.au>
+ * @property int FieldID
+ * @property string Name
+ * @property string Type
+ * @property int IsMandatory
+ * @property int ShowInForm
+ * @property int Length
+ * @property int Size
+ * @property int RowSize
+ * @property string DefaultValue
+ * @property string ValuesArray
+ * @property string DefaultsArray
+ * @property string AllowedFileTypes
+ * @property string AddressType
+ * @property string TimeFormat
+ * @property string DateValidation
+ * @property string DisplayOrder
+ * @method Vision6List Lists
+ */
+class Vision6Field extends DataObject
+{
     protected static $db = array(
         "FieldID" => "Int", // The ID of the Field.
         "Name" => "Varchar(100)", // The name of the Field in the List.
@@ -9,6 +32,7 @@ class Vision6Field extends DataObject {
         "ShowInForm" => "Int(1)", // A value indicating whether the Field is displayed in subscription Forms by default.
         "Length" => "Int", // The maximum length for values stored in a text Field.
         "Size" => "Int",
+        "RowSize" => "Int",
         "DefaultValue" => "Varchar", // The default value for text, comment, decimal, currency and date Fields. For date Fields can be current or a date in ISO format (e.g. "2001-03-17").
         "ValuesArray" => "Text", // A comma separated list of values for drop-down list, radio and checkbox Fields.
         "DefaultsArray" => "Varchar", // The default value for drop-down list, radio and checkbox Fields.
